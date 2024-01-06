cf04be6a16b49730c1fdba4afbf2a8fda48f2487
Flustivs <rune170206@gmail.com>
2024-01-06 23:23:13 +0100

    New button on ToDoList

---
+++ Runes_Bio/Pages/CalculateFood.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Runes_Bio.Pages
+{
+    public class CalculateFoodModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Runes_Bio/wwwroot/js/ToDo.js
+++ Runes_Bio/wwwroot/js/ToDo.js
@@ -1,7 +1,20 @@
 ﻿function PickUpTrash() {
-    var button = document.querySelector(".button");
+    var button = document.getElementById("ButtonTrash");
     button.classList.add("fade-out");
     setTimeout(function () {
         window.location.href = "/Pick-Trash";
+    }, 1000);
+}
+function Calculate() {
+    var button = document.getElementById("ButtonCalculate");
+    button.classList.add("fade-out");
+    setTimeout(function () {
+        let num = Math.random(0, 2);
+        if (num === 0) {
+            window.location.href = "/CalculateTickets";
+        }
+        else {
+            window.location.href = "/CalculateFood";
+        }
     }, 1000);
 }
